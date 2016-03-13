9e545d148c807fe077f6085a0355ed99985dc027
lusarz <lukasz.usarz@gmail.com>
2016-03-13 15:44:32 +0100

    Login page - submit instead ng-click.

--- backend/data-scripts/addTodo.sh
+++ backend/data-scripts/addTodo.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-type: application/json" -d @$1 http://localhost:3000/todos
+curl -X POST -H "Content-type: application/json" -H "Authorization: Bearer kpU7zFDWC3EhrMP2bmkqbhNrhcZWRYZDkrDKrMDG" -d @$1 http://localhost:9000/api/todos
