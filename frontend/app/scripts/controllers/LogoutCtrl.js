f6ec086666e534c7379ebc81d51bf289f16ffdd4
lusarz <lukasz.usarz@gmail.com>
2016-03-14 22:07:13 +0100

    Improve logo styles, logout.

---
+++ frontend/app/scripts/controllers/LogoutCtrl.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc function
+     * @name app.controller:LogoutCtrl
+     * @description
+     * # LogoutCtrl
+     * Controller of the app
+     */
+    function LogoutCtrl(SecurityFactory, $state) {
+        SecurityFactory.logout();
+        $state.go('login');
+    }
+
+    angular.module('app')
+        .controller('LogoutCtrl', LogoutCtrl);
+
+    LogoutCtrl.$inject = ['SecurityFactory', '$state'];
+
+})();
--- frontend/app/scripts/directives/navbarToggle.js
+++ frontend/app/scripts/directives/navbarToggle.js
@@ -1,26 +1,23 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('app')
-    .directive('navbarToggle', navbarToggle);
+    angular.module('app')
+        .directive('navbarToggle', navbarToggle);
 
-  function navbarToggle() {
-    return {
-      restrict: 'C',
-      replace: false,
-      transclude: false,
-      link: function (scope, elem) {
-        var aside = $('aside');
-
-        elem.bind('click', function (e) {
-          console.log(e);
-          e.preventDefault();
-          e.stopPropagation();
-          aside.toggleClass('collapsed');
-        });
-      }
-    };
-  }
+    function navbarToggle() {
+        return {
+            restrict: 'C',
+            replace: false,
+            transclude: false,
+            link: function (scope, elem) {
+                elem.bind('click', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    $('aside').toggleClass('collapsed');
+                });
+            }
+        };
+    }
 
 
 })();
