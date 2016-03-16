0e2e605ad868bbce55f4d453c21c8eb805b798e4
lusarz <lukasz.usarz@gmail.com>
2016-03-16 20:14:12 +0100

    Write some tests.

--- backend/test/unit/user.routes.spec.js
+++ backend/test/unit/user.routes.spec.js
@@ -4,7 +4,7 @@
   var server = require('../../../server.js');
   var request = require('supertest').agent(server.app.listen());
 
-  var User = require('../../app/models/user.model')
+  var User = require('../../app/models/user.model');
 
   describe('User endpoint endpoint', function () {
     before(function () {
@@ -13,7 +13,7 @@
 
     it('should return empty list', function (done) {
       request.get('/api/todos')
-        .expect(200)
+        .expect(401)
         .expect("[]")
         .end(function (err, res) {
           if (err)
@@ -23,10 +23,10 @@
         });
     });
 
-    it('should not add without authentication', function (done) {
+    it('should not add todo when user is not authenticated', function (done) {
       var todo = {};
       request.post('/api/todos').send(todo)
-        .expect(400)
+        .expect(401)
         .expect("[]")
         .end(function (err, res) {
           if (err)
@@ -34,6 +34,55 @@
           else
             done();
         });
+    });
+
+    describe('Login method', function () {
+      it(' should return 400 when bad credentials sended', function (done) {
+        var credentials = {email: 'test@onet.pl', password: 'abcdef'};
+        request.post('/api/user/login').send(credentials)
+          .expect(400)
+          .end(function (err, res) {
+            if (err)
+              done(err);
+            else
+              done();
+          });
+      });
+    });
+
+    describe('Register method', function () {
+      var validRegistrationData = {
+        firstName: 'Jan',
+        lastName: 'Nowak',
+        email: 'jan.nowak@gmail.com',
+        password: 'thisIsPassword123!'
+      };
+
+      it(' should return 200', function (done) {
+        request.post('/api/user/register').send(validRegistrationData)
+          .expect(200)
+          .end(function (err, res) {
+            console.log(res);
+            if (err) {
+              done(err);
+            } else {
+              done();
+            }
+          });
+      });
+
+      it(' should login after registration', function (done) {
+        var credentials = {email: validRegistrationData.email, password: validRegistrationData.password};
+        request.post('/api/user/login').send(credentials)
+          .expect(200)
+          .end(function (err, res) {
+            if (err)
+              done(err);
+            else
+              done();
+          });
+      });
+
     });
   });
 
