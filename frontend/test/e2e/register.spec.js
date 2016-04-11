b5bbf831b9dad89a6208bfe430ec1b841d5647d6
lukasz.usarz <lukasz.usarz@gmail.com>
2016-04-11 17:58:49 +0200

    Prepare some e2e tests.

--- frontend/test/e2e/register.spec.js
+++ frontend/test/e2e/register.spec.js
@@ -3,7 +3,7 @@
 
   describe('Registration form', function () {
     beforeAll(function () {
-      browser.get('/');
+      browser.get('/#/register');
     });
 
     describe('On startup', function () {
@@ -13,6 +13,20 @@
         expect(header.getText()).toEqual('Register');
       });
     });
-  });
 
+    describe('Validation', function () {
+      it('too short password message should be present', function () {
+        element(by.model('vm.registrationData.password')).sendKeys('qwert');
+        expect(element(by.css('[name="password"]+.help-block [ng-message="minlength"]')).isPresent()).toBeTruthy();
+      });
+
+      it('Email is required message should be present', function () {
+        var emailElement = element(by.model('vm.registrationData.email'));
+        emailElement.sendKeys('costam');
+        emailElement.clear();
+
+        expect(element(by.css('[name="email"]+.help-block [ng-message="required"]')).isPresent()).toBeTruthy();
+      });
+    });
+  });
 })();
